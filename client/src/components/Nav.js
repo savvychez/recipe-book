c09e7a0c11eebe9ccdac9f5ee257b5c0c8cc4ccb
RadioactiveArrow <36655291+RadioactiveArrow@users.noreply.github.com>
2020-07-13 01:05:38 -0500

    Update Nav.js
    switched nav's h1 to new title

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Nav() {
   return (
     <nav className="nav">
-      <Link className="router-link no-hover" to="/"><h1 className="logo-icon">Momster's&nbsp;Recipes </h1></Link>
+      <Link className="router-link no-hover" to="/"><h1 className="logo-icon">RecipeBook</h1></Link>
       <ul className="nav-links">
         <Link className="router-link" to="/" ><li className="nav-item">Recipes</li></Link>
         <Link className="router-link" to="/compose" ><li className="nav-item">Compose</li></Link>
